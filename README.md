93cf8227af0ba0ea83f7cb8def402c398ada050c
moonitro <moonitro@proton.me>
2022-10-08 18:18:12 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # react-typescript-template
 A React Typescript Template with SCSS Styles
+
+# How to get started
+> git clone repo & remove the .git folder & git init your new project
---
+++ js/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div id="wrapper">
+      <h1>
+        <span className="red">Hello World</span> from <span className="blu">React</span> with <span className="blu">Typescript</span>!
+      </h1>
+    </div>
+
+  );
+};
+
+export default App;
